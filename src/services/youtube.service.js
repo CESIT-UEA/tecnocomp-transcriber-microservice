20cbf9b4c293bae64d85c44c67bdad0179bcd97d
KaueWallace <kauewall8@gmail.com>
2026-06-18 17:15:09 -0400

    testando baixar audio

--- src/services/youtube.service.js
+++ src/services/youtube.service.js
@@ -26,7 +26,6 @@ async function baixarAudio(url, destino) {
    await run(
         `yt-dlp \
         --cookies /app/cookies.txt \
-        --extractor-args "youtube:player_client=android" \
         -x \
         --audio-format mp3 \
         -o "${destino}" \
