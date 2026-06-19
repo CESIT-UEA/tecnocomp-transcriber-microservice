c6bc3ae7fec1046e216abae6d4410132ec0d768b
KaueWallace <kauewall8@gmail.com>
2026-06-18 23:34:03 -0400

    teste

--- src/services/youtube.service.js
+++ src/services/youtube.service.js
@@ -24,8 +24,8 @@ function run(comando) {
 async function baixarAudio(url, destino) {
 
    await run(
-    `yt-dlp --js-runtimes node:/usr/local/bin/node --extractor-args "youtube:getpot_bgutil_baseurl=http://bgutil-provider:4416" --ap-mhtml-filter "" --ap-proxy "https://invidious.nerdvpn.de" -x --audio-format mp3 -o "${destino}" "${url}"`
-    );
+    `yt-dlp --proxy "https://invidious.nerdvpn.de" --js-runtimes node:/usr/local/bin/node --extractor-args "youtube:getpot_bgutil_baseurl=http://bgutil-provider:4416" -x --audio-format mp3 -o "${destino}" "${url}"`
+);
 
     return destino;
 }
